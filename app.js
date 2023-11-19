8abb3c99d510a78a106660ea65afbc9f22725a95
soyoung <thgml1444@naver.com>
2023-11-20 00:17:15 +0900

    javascript로 사용자 입력을 받아 데이터 추가

---
+++ app.js
@@ -0,0 +1,102 @@
+const newPeriodFormEl = document.getElementsByTagName("form")[0];
+const startDateInputEl = document.getElementById("start-date");
+const endDateInputEl = document.getElementById("end-date");
+const pastPeriodContainer = document.getElementById("past-periods");
+
+const STORAGE_KEY = "period-tracker";
+newPeriodFormEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Get the start and end dates from the form.
+  const startDate = startDateInputEl.value;
+  const endDate = endDateInputEl.value;
+
+  // Check if the dates are invalid
+  if (checkDatesInvalid(startDate, endDate)) {
+    // checkDates의 데이터 검증이 옳지 못하면 return을 하게 된다.
+    return;
+  }
+
+  // Store the new period in our client-side storage.
+  storeNewPeriod(startDate, endDate);
+
+  // Refresh the UI.
+  renderPastPeriods();
+
+  // Reset the form.
+  newPeriodFormEl.reset();
+});
+
+// 입력데이터 유효성 검증 함수
+function checkDatesInvalid(startDate, endDate) {
+  // Check that end date is after start date and neither is null.
+  if (!startDate || !endDate || startDate > endDate) {
+    newPeriodFormEl.reset();
+    return true;
+  }
+  return false;
+}
+
+// 입력받은 period data를 배열에 정렬 및 추가하는 함수
+function storeNewPeriod(startDate, endDate) {
+  // Get data from storage.
+  const periods = getAllStoredPeriods();
+
+  // Add the new period objet to the end of the array of period objects.
+  periods.push({ startDate, endDate });
+
+  // Sort the array so that periods are ordered by start date, from newest to oldest.
+  periods.sort((a, b) => {
+    return new Date(b.startDate) - new Date(a.startDate);
+  });
+
+  // Store the updated array back in the storage.
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(periods));
+}
+
+// 로컬스토리지에 데이터가 있는지 확인하고 데이터를 반환하는 함수
+function getAllStoredPeriods() {
+  const data = window.localStorage.getItem(STORAGE_KEY);
+  const periods = data ? JSON.parse(data) : [];
+  console.log(periods)
+  console.dir(periods)
+  return periods;
+}
+
+// 로컬스토리지에 데이터를 확인한 뒤 화면에 렌더링하는 함수
+function renderPastPeriods() {
+  const periods = getAllStoredPeriods();
+  if (periods.length === 0) {
+    return;
+  }
+  pastPeriodContainer.innerHTML = "";
+
+  const pastPeriodHeader = document.createElement("h2");
+  pastPeriodHeader.textContent = "Past periods";
+
+  const pastPeriodList = document.createElement("ul");
+  periods.forEach((period) => {
+    const periodEl = document.createElement("li");
+    periodEl.textContent = `From ${formatDate(
+      period.startDate,
+    )} to ${formatDate(period.endDate)}`;
+    pastPeriodList.appendChild(periodEl);
+  });
+
+  pastPeriodContainer.appendChild(pastPeriodHeader);
+  pastPeriodContainer.appendChild(pastPeriodList);
+}
+
+// 입력받은 생리주기 date의 형태를 만들어주는 함수
+function formatDate(dateString) {
+  // Convert the date string to a Date object.
+  const date = new Date(dateString);
+
+  // Format the date into a locale-specific string.
+  // include your locale for better user experience
+  return date.toLocaleDateString("en-US", { timeZone: "UTC" });
+} 
+
+// 사용자가 데이터를 입력하지 않아도, 로컬 스코리지에 데이터가 있는 경우 
+// 데이터를 볼 수 있도록 렌더링 함수를 호출한다 
+renderPastPeriods();
